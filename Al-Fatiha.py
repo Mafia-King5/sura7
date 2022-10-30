01b57fb4aeba8fb2e87d5bb4c356aa80119d024d
Mafia-King5 <116032756+Mafia-King5@users.noreply.github.com>
2022-10-30 18:32:10 +0600

    Add files via upload

---
+++ Al-Fatiha.py
@@ -0,0 +1,176 @@
+#Tools Created By All Hacking Tools
+#AUTHOR : All Hacking Tools
+#Tools : Al-Fatiha
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+ 
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+ 
+os.system('clear')
+print
+ 
+print
+
+psb('\x1b[1;32mBismillahir Rahmaner Rahim...........................')
+ 
+print("""
+
+\033[0;93m
+\033[0;93m /$$$$$$  /$$$$$$  /$$        /$$$$$$  /$$      /$$
+\033[0;93m|_  $$_/ /$$__  $$| $$       /$$__  $$| $$$    /$$$
+\033[0;93m  | $$  | $$  \__/| $$      | $$  \ $$| $$$$  /$$$$
+\033[0;93m  | $$  |  $$$$$$ | $$      | $$$$$$$$| $$ $$/$$ $$
+\033[0;93m  | $$   \____  $$| $$      | $$__  $$| $$  $$$| $$
+\033[0;93m  | $$   /$$  \ $$| $$      | $$  | $$| $$\  $ | $$
+\033[0;93m /$$$$$$|  $$$$$$/| $$$$$$$$| $$  | $$| $$ \/  | $$
+\033[0;93m|______/ \______/ |________/|__/  |__/|__/     |__/
+\033[0;93m
+
+""")
+
+print("\033[1;96mThis Tool's Free By All Hacking Tools")
+
+psb('\x1b[1;92mSURA AL-FATIHA START PLEASE WAIT.....')
+ 
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+ 
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+    os.system('python2 C')
+ 
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+os.system('clear')
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [
+ ('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+ 
+def exb():
+    print
+    os.sys.exit()
+ 
+ 
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+ 
+ 
+def t():
+    time.sleep(1)
+ 
+ 
+def cb():
+    os.system('clear')
+    
+logo = """                                                                     
+\033[0;94m \\\\\\\\\\\\\\\\\\\\\WELCOME TO AHT////////////////
+\033[0;96m=============================================
+\033[0;92m            _        ______    _   _ _           
+\033[0;92m      /\   | |      |  ____|  | | (_) |          
+\033[0;92m     /  \  | |______| |__ __ _| |_ _| |__   __ _ 
+\033[0;92m    / /\ \ | |______|  __/ _` | __| | '_ \ / _` |
+\033[0;92m   / ____ \| |      | | | (_| | |_| | | | | (_| |
+\033[0;92m  /_/    \_\_|      |_|  \__,_|\__|_|_| |_|\__,_|
+\033[0;92m
+\033[0;96m =============================================
+\033[0;93m AUTHOR       : All Hacking Tools
+\033[0;94m PAGE         : All Hacking Tools
+\033[0;95m YOUTUBE      : All Hacking Tools
+\033[0;96m GITHUB       : AHT1-AHT1
+\033[0;96m =============================================                                           
+"""
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+ 
+def menu():
+    os.system('clear')
+    print logo
+    print ''
+    print '\033[0;92mSURA AL-FATIHA'
+    print '\033[0;92m'
+    print 50* '-'
+    print '\033[0;92m[1]  \x1b[1;92mAl-Fatiha'
+    print '\033[0;91m[0]  \x1b[1;91mExit'
+    print '\033[0;92m'
+    print 50 * '-'
+    action()
+    
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\033[0;92m\n> ')
+    if bch == '':
+        print
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print 50* '-'
+        print ''
+        print 'Al-Fatiha'
+        print''
+        try:
+            c = raw_input('\033[0;92mChoose Code : ')
+            
+ 
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '0':
+        exb()
+    else:
+        print
+        action()
+    xxx = str(len(id))
+    psb('[\xe2\x9c\x93] Bismillahir Rahmanir Rohim...')
+    time.sleep(1.9)
+    psb('[\xe2\x9c\x93] Alhamdulillahi Rabbil Alamin...')
+    time.sleep(1.9)
+    psb('[\xe2\x9c\x93] Arrahmanir Rahim...')
+    time.sleep(1.9)
+    psb('[\xe2\x9c\x93] Maliki Eyawmiddin...')
+    time.sleep(1.9)
+    psb('[\xe2\x9c\x93] Eya Kana Budu Eya Kanastain...')
+    time.sleep(1.9)
+    psb('[\xe2\x9c\x93] Ehdinas Siratal Mustakim...')
+    time.sleep(1.9)
+    psb('[\xe2\x9c\x93] Siratallazina Anamta Alaihim Gairil Magdubi Alaihim Waladdin...')
+    time.sleep(1.9)
+    psb('[\xe2\x9c\x93] Amin...')
+    time.sleep(1.9)
+    psb(50 * '-')
+    time.sleep(0.5)
+    print
+    50 * '-'
+    print
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 Al-Fatiha.py')
+ 
+ 
+if __name__ == '__main__':
+    menu()
